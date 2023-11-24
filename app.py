59cb8c8610d2de9590af14761e1e060011e91e5e
Rikiya Yamamoto <rikiyaeduarteyamamoto@gmail.com>
2023-11-24 20:28:44 +0800

    Remove unused import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson import ObjectId
 
